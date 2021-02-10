85b55196c946be885b956f42e128f5955558a0dc
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-02-11 09:27:05 +1000

    Delete logger.py

--- logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logging.basicConfig(
-	format=("%(filename)s [Line:%(lineno)d]# " +
-			"%(levelname)-8s [%(asctime)s] %(message)s"),
-	level=logging.INFO)
-
-db_log = logging.getLogger("db_log")
-main_log = logging.getLogger("main_log")
