031f49a52c4e394b9464819861f7a52aa484f292
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-02-13 20:54:01 +1000

    Update market.py

--- market.py
+++ market.py
@@ -46,9 +46,9 @@ def antispam(msg):
 	else:
 		textspam=msg.text.lower()
 	
-	mywords = ("zwzff", "wa.me", "www", "http", "t.me", "www", "0")
+	keywords = ("zwzff", "wa.me", "www", "http", "t.me", "www", "0")
 	
-	if any(x in mystring for x in mywords)==True:
+	if any(word in textspam for word in keywords):
 		bot.delete_message(msg.chat.id, msg.message_id)
 
 
