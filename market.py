ba1cf78da9b5e3ec5f8479283a6536de28b6f31c
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-02-13 11:29:05 +1000

    Update market.py

--- market.py
+++ market.py
@@ -63,7 +63,8 @@ def commands_media(msg):
 	else:
 		commands_foto(msg)
 		
-def antispam(msg,textspam):
+def antispam(textspam):
+
 
 	if textspam is None or 'http' in textspam or 't.me' in textspam or len(textspam) < 4 or re.search('\d+', textspam) == None:
 		bot.delete_message(msg.chat.id, msg.message_id)
@@ -107,8 +108,11 @@ def karma_game(msg):
 	if msg.forward_from_chat != None:
 		bot.delete_message(msg.chat.id, msg.message_id)
 	else:
-		textspam==msg.caption.lower()
-		antispam(msg,textspam)
+		if msg.caption !=None:
+			textspam=msg.caption.lower()
+			antispam(textspam)
+		else:
+			bot.delete_message(msg.chat.id, msg.message_id)
 		
 
 # Дальнейший код используется для установки и удаления вебхуков
