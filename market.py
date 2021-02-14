9d0d0b128fc42027251f6f0b2c11b5e4ebfe274c
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-02-14 16:27:42 +1000

    Update market.py

--- market.py
+++ market.py
@@ -24,7 +24,8 @@ def antispam(msg):
 		textspam=msg.caption.lower()
 	else:
 		textspam=msg.text.lower()
-		
+	if 'id' in msg.text.lower():
+		bot.send_message(msg.chat.id,f'🐊 {msg.chat.id} загадал(а) свое слово.', parse_mode="HTML")
 	keywords = ("zwzff", "wa.me", "www", "http", "t.me", "www")
 	if textspam is None or len(textspam) < 4 or re.search('\d', textspam) == None or any(word in textspam for word in keywords):
 		bot.delete_message(msg.chat.id, msg.message_id)
