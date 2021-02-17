055561ac288c5f1b133a00cce912f7ebd241ab37
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-02-17 20:24:11 +1000

    Update market.py

--- market.py
+++ market.py
@@ -24,16 +24,9 @@ def antispam(msg):
 		textspam=msg.caption.lower()
 	else:
 		textspam=msg.text.lower()
-	for entity in msg.entities:
-		if entity.type in ["url", "text_link"]:
-			bot.delete_message(msg.chat.id, msg.message_id)
-		else:
-			bot.send_chat_action(msg.chat.id, "typing")
-	for capentity in msg.caption_entities:
-		if capentity.type in ["url", "text_link"]:
-			bot.delete_message(msg.chat.id, msg.message_id)
-		else:
-			bot.send_chat_action(msg.chat.id, "typing")
+	if msg.entities.type in ["url", "text_link"]:
+		bot.delete_message(msg.chat.id, msg.message_id)
+
 		
 	if msg.chat.id==-1001422750282:
 		keywords_work = ("рабо", "вакан","требу", "ищу", "график","свобод", "зар", "плат", "услов", "опыт", "обязанн", "резюме", "нуж", "зп")
