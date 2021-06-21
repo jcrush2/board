64f00b49c9399be63b5d82b34b7fe749f3920785
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-06-22 08:52:13 +1000

    Update market.py

--- market.py
+++ market.py
@@ -21,13 +21,13 @@ def otzyv(msg):
 def antispam(msg):
 	try:
 		for entity in msg.entities:
-			if entity.type in ["url", "text_link", "mention"]:
+			if entity.type in ["url", "text_link"]:
 				bot.delete_message(msg.chat.id, msg.message_id)
 	except Exception:
 		print("Error!")
 	try:
 		for entityc in msg.caption_entities:
-			if entityc.type in ["url", "text_link", "mention"]:
+			if entityc.type in ["url", "text_link"]:
 				bot.delete_message(msg.chat.id, msg.message_id)
 	except Exception:
 		print("Error!")
@@ -70,7 +70,7 @@ def reply_text(msg):
 @bot.message_handler(content_types=['text'])	
 def antispam_text(msg):
 	user = bot.get_chat_member(msg.chat.id, msg.from_user.id)
-	if user.status == 'creator'  :
+	if user.status == 'creator' or user.is_anonymous == True :
 		return
 	else:
 		if msg.forward_from_chat != None:
