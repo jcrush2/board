f0d6b202fa5b941dfc09feafd8cc97f4d7934b56
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-06-30 21:55:54 +1000

    Update market.py

--- market.py
+++ market.py
@@ -45,7 +45,7 @@ def antispam(msg):
 		else:
 			bot.delete_message(msg.chat.id, msg.message_id)
 
-	keywords = ("zwzff", "wa.me")
+	keywords = ("zwzff", "wa.me","275o")
 	if textspam is None or len(textspam) < 4 or re.search('\d', textspam) == None or any(word in textspam for word in keywords):
 		bot.delete_message(msg.chat.id, msg.message_id)
 	else:
