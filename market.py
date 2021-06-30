975e5cec4ec54e87c4753bded65351e25f82b14f
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-06-30 22:02:33 +1000

    Update market.py

--- market.py
+++ market.py
@@ -45,13 +45,9 @@ def antispam(msg):
 		else:
 			bot.delete_message(msg.chat.id, msg.message_id)
 
-	keywords = ("zwzff", "wa.me")
-	keyban = ("275o")
+	keywords = ("zwzff", "wa.me","275o")
 	if textspam is None or len(textspam) < 4 or re.search('\d', textspam) == None or any(word in textspam for word in keywords):
 		bot.delete_message(msg.chat.id, msg.message_id)
-	if any(word in textspam for word in keyban):
-		bot.delete_message(msg.chat.id, msg.message_id)
-		bot.restrict_chat_member(msg.chat.id, msg.from_user.id, until_date=time()+86400)
 	else:
 		otzyv(msg)
 		
