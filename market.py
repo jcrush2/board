ffae1b7a973ef2ca858e201204ac782a5e99d8c4
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-10-31 22:47:38 +1000

    Update market.py

--- market.py
+++ market.py
@@ -65,9 +65,7 @@ def antispam(msg):
 		
 	if msg.chat.id==-1001422750282:
 		keywords_work = ("рабо", "вакан","требу", "труд", "ищу", "занятост", "график","свобод", "зар", "плат", "услов", "опыт", "обязанн", "резюме", "нуж", "зп", "приглаш")
-		if any(word in textspam for word in keywords_work):
-			print("Error!")
-		else:
+		if not any(word in textspam for word in keywords_work):
 			bot.delete_message(msg.chat.id, msg.message_id)
 	else:
 		otzyv(msg)
