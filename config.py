be2515510935b8c0f06b4aaa4612cc88c12e2324
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2021-11-27 17:58:42 +1000

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Бот в текущей сборке расчитан на работу с heroku.com
 # Это ссылка на приложение, для установления вебхука
-url = "https://khabarovskiybot2.herokuapp.com/bot"
+url = "https://khabarovskiybot3.herokuapp.com/bot"
 
 # Имя бота (то, которое @)
 bot_name = "khabarovskiybot"
