3d702929902273c31ffd56c2e5efae4544d516fa
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2022-03-10 11:52:36 +1000

    Update market.py

--- market.py
+++ market.py
@@ -11,9 +11,11 @@ import config
 TELEGRAM_API = os.environ["telegram_token"]
 bot = telebot.TeleBot(TELEGRAM_API)
 	
-def otzyv(msg):        
+def otzyv(msg):
+	nam=msg.from_user.first_name.replace('"', '')
+	nam=nam.strip()
 	keyboard = types.InlineKeyboardMarkup()
-	url_button = types.InlineKeyboardButton(text=f"💬 Оставить отзыв", url=f"https://khabara.ru/tg/{msg.from_user.id}-id.html#{msg.from_user.first_name}")
+	url_button = types.InlineKeyboardButton(text=f"💬 Оставить отзыв", url=f"https://khabara.ru/tg/{msg.from_user.id}-id.html#{name}")
 	
 	keyboard.add(url_button)
 	bot.reply_to(msg, f'ℹ️ Объявление от <a href="tg://user?id={msg.from_user.id}">{msg.from_user.first_name}</a>', parse_mode="HTML", reply_markup=keyboard)
