10cf71b24dd47c348cf7eb4b0249ba0d3e0664c7
jcrush2 <32280851+jcrush2@users.noreply.github.com>
2022-03-10 14:00:07 +1000

    Update market.py

--- market.py
+++ market.py
@@ -23,8 +23,8 @@ def otzyv(msg):
 			
 		
 def antispam(msg):
-	if msg.sender_chat!=None:
-		bot.ban_chat_member(msg.chat.id, msg.sender_chat.id)
+	if msg.sender_chat:
+		ban_chat_sender_chat(msg.chat.id, msg.sender_chat.id)
 	if msg.caption !=None:
 		textspam=msg.caption.lower()
 	else:
